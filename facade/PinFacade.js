44e4f9c60c5a64c8805284d767f9afccd05944b4
Janus <janusudsen@yahoo.com>
2017-11-14 07:43:49 +0100

    Update PinFacade.js

--- facade/PinFacade.js
+++ facade/PinFacade.js
@@ -3,7 +3,16 @@ const mongoose = require("mongoose");
 var Location = mongoose.model("location");
    
 function pin(name, txt, cords) {
-  Location.insertMany({ userName: name, text: txt, loc: { type: "Point", coordinates: cords }});
+  let inserts = [
+    { userName: name, text: txt, loc: { type: "Point", coordinates: cords }}
+  ]
+  Location.insertMany(inserts, function (err, d) {
+    if (err) {
+      console.log("UPPS: ", err);
+    }
+    console.log("Inserted :" + d);
+    mongoose.connection.close();
+  })
 }
 
 function findPins(callback){
