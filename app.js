afc65acf1d18657ec74bc80f9ec8e07b6367c21c
Janus <janusudsen@yahoo.com>
2017-11-14 06:13:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-require("./models/Locations");
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const mongoose = require("mongoose");
-Location = mongoose.model("location");
+const PinFacade = require("./facade/PinFacade");
+
 
 
 
 app.get('/', function (req, res) {
   //recieve JSON with markers/pins
-  findPins(function(response){
+  PinFacade.findPins(function(response){
     res.send(JSON.stringify(response,null,""));
   });
 })
@@ -20,17 +19,5 @@ app.listen(PORT, function () {
 
 app.post('/pin', function(req, res){
   let json = JSON.parse(req.body);
-  pin(json.userName, json.text, json.coordinates);
+  PinFacade.pin(json.userName, json.text, json.coordinates);
 })
-
-
-function pin(name, txt, cords) {
-  Location.insertOne({ userName: name, text: txt, loc: { type: "Point", coordinates: cords }});
-}
-
-function findPins(callback){
-  let query = Location.find()
-  query.exec(function (err, docs) {
-    callback(err,docs);
-  })
-}
